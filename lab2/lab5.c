07d0ecbf803dc8f73812a67e2594a076d457e4df
Teerapat <teerapat123@gmail.com>
2025-12-08 11:10:35 +0700

    uplab2 -8

--- lab2/lab5.c
+++ lab2/lab5.c
@@ -3,7 +3,7 @@
 
 int main(){
     char ch1[10], ch2[10],ch3[10];
-    int ch1_d , ch2_d , ch3_d; 
+    int ch1_d , ch2_d , ch3_d,total = 0; 
     
     printf("Enter length of side A: ");
     fgets(ch1,10,stdin);
@@ -15,6 +15,19 @@ int main(){
     ch1_d = atoi(ch1);
     ch2_d = atoi(ch2);
     ch3_d = atoi(ch3);
+
+    if (ch1_d <= 0 || ch2_d <= 0 || ch3_d <=0 || ch1_d + ch2_d <= ch3_d || ch1_d + ch3_d <= ch2_d || ch2_d + ch3_d <= ch1_d ){
+        printf("Triangle type is invalid.");
+    }
+    else if (ch1_d == ch2_d && ch2_d == ch3_d){
+        printf("Triangle type is equilateral.");
+    }
+    else if (ch1_d == ch2_d || ch1_d == ch3_d || ch2_d == ch3_d){
+        printf("Triangle type is isosceles.");
+    }
+    else{
+        printf("Triangle type is scalene.");
+    }
 
     
 
