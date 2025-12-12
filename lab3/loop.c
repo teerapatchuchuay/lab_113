81bac08c2f0984cbd9f71bc3610db63a426b3733
Teerapat <teerapat123@gmail.com>
2025-12-12 13:48:16 +0700

    up study loop.c success

--- lab3/loop.c
+++ lab3/loop.c
@@ -23,8 +23,52 @@ int main(){
     // }
     // ลดลงก่อน ตรวจสอบ ว่าเป็น 0 ไหม จบที่ 1
 
+
+
     // for (int i = 0 ; i < 10 ; i++){
     //     printf("%d",i);
     // } เริ่ม 0 ถึง 9
+
+
     
+    // for (int i = 0 ; i < 10 ; i++){  
+    //     printf("%d\n",'A' + i); สามารถใช้ การบวก int กับ str ได้  โดยสามารภเป็นได้ทั้ง ตัวเลขหรือตัวอักษร โดย ใช้ %c or %d
+    // }
+
+
+    // double temp = 35.0;
+    // while (1){
+    //     if (temp <= 25.0) break;{
+    //         printf("te %.1lf",temp);
+    //         temp -= 0.3;
+    //     }
+    // }
+
+    // char string[] = "hello";
+    // int i = 0;
+    // while (1){
+    //     if (string[i] == '\0') break;{
+    //         printf("[%c]",string[i++]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // double temp = 35.0;
+    // do{
+    //     printf("tem: %.1lf\n",temp);
+    //     temp -= 0.3;
+
+    // }while(temp > 25); ทำให้ do ก่อน เช็ค while 
+
+    // char input[16];
+    // int x;
+    // int total = 0;
+    // for (; 1;){
+    //     fgets(input,16,stdin);
+    //     x = atoi(input);
+    //     if (x <= 0) break;{
+    //         total += x;
+    //     }
+    //     printf("%d\n",total);
+    // }
 }
