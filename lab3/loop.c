39078894603c63572de0bee0535ccc5083ed19c4
Teerapat <teerapat123@gmail.com>
2025-12-12 13:26:44 +0700

    up lab3

---
+++ lab3/loop.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // int i ;
+    // i = 0 ;
+    // while (i < 10){
+    //     printf("%d\n",i);
+    //     i++;
+    // }
+    // ได้  9
+
+
+    // int n = 6;
+    // while (n--){
+    //     printf("%d\n",n);
+    // }
+    // ตรวจสอบว่าเป็น 0 ไหมก่อนลดลง 1  จบ 0
+
+    // int n = 6 ;
+    // while (--n){
+    //     printf("%d\n",n);
+    // }
+    // ลดลงก่อน ตรวจสอบ ว่าเป็น 0 ไหม จบที่ 1
+
+    // for (int i = 0 ; i < 10 ; i++){
+    //     printf("%d",i);
+    // } เริ่ม 0 ถึง 9
+    
+}
