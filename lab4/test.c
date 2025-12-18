573970ff45f3ffb28f86a8fdc929d4e61986aff3
teerapatchuchuay <teerapatchuchuay4240@gmail.com>
2025-12-18 21:13:19 +0700

    uplab4

---
+++ lab4/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    int a = 4 , x = 2 ,sum() , to;
+
+    if (a % 2 == 0){
+        to = sum(a,x);
+    }else{
+        to = 1;
+    }
+printf("%d",to);
+}
+
+int sum(int a,int b){
+    return a+b;
+}
